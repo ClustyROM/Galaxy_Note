309876baa6f9436795b96165e213e7f121dfaa72
franciscofranco <franciscofranco.1990@gmail.com>
2012-02-19 18:07:14 +0000

    Increase min GPU frequency from 166mhz to 200mhz. Disable MALI tracking debug

--- drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
+++ drivers/media/video/samsung/mali/platform/orion-m400/mali_platform_dvfs.c
@@ -83,7 +83,7 @@ int mali_dvfs_control=0;
 mali_dvfs_table mali_dvfs[MALI_DVFS_STEPS]={
 #ifdef CONFIG_CPU_S5PV310_EVT1
 #ifdef CONFIG_S5PV310_ASV
-                   /*step 0*/{160  ,1000000    ,1000000},
+                   /*step 0*/{200  ,1000000    ,1000000},
                    /*step 1*/{267  ,1000000    ,1100000} };
 #else
 		   /*step 0*/{160  ,1000000    , 950000},
